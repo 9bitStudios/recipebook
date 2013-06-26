881b43a2ebd145b31184262668ccfcdf475e4986
Ian C <9bitStudios@gmail.com>
2013-06-26 06:05:41 -0700

    Adding home link... Adding random string helper function... Updating templates...

--- app/libs/helper/helper.js
+++ app/libs/helper/helper.js
@@ -6,6 +6,19 @@ define(['underscore', 'backbone'], function(_, Backbone){
 			return $('<div/>').text(value).html();
 		},
 		
+		randomString: function(strLength) {
+		
+			if(typeof strLength === 'undefined') 
+				strLength = 5;
+		
+			var text = "";
+			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+			for( var i=0; i < strLength; i++ )
+				text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+			return text;		
+		},
 		
 		createCookie: function(name,value,days) {
 			if (days) {
