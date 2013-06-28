4a9f808fcfccad4ab157c60e436d9e508a11c367
Ian C <9bitStudios@gmail.com>
2013-06-28 13:30:28 -0700

    Adding in LoadingView.js...

---
+++ app/views/LoadingView.js
@@ -0,0 +1,32 @@
+define(['jquery',
+	'underscore', 
+	'backbone', 
+	'globals',
+	], function($, _, Backbone, globals){
+
+		
+    var LoadingView = Backbone.View.extend({
+
+	tagName: 'div',
+	
+	className: 'loading',
+
+	targetElement: 'body',
+
+	initialize: function(options){
+	    if(options && options.hasOwnProperty('target'))
+		this.targetElement = options.target;
+	    
+	    this.render();
+	},
+	render: function() {
+	    this.$el.text('Loading');
+	    this.$el.prependTo(this.targetElement);
+	}
+    });
+    
+    return LoadingView;
+	
+});
+
+
