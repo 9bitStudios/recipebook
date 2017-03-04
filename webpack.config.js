eb880e77ea2f7445b795365db9920de8aa6f41bb
Ian C <9bitStudios@gmail.com>
2017-03-04 07:17:55 -0800

    Version updates to React 15.4 and webpack 2.2.1...

--- webpack.config.js
+++ webpack.config.js
@@ -10,21 +10,25 @@ module.exports = {
     },
     watch: true,
     resolve: {
-        root: [
-            path.resolve(__dirname  + '/react/src')
-        ]
+        modules: [path.resolve(__dirname, 'react/src'), "node_modules"]
     },    
     module: {
-        loaders: [{
+        rules: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: 'babel',
+            loader: 'babel-loader',
             query:{
                 presets: ['es2015', 'react']
             }
         }]
     },   
-    plugins:[
-        new webpack.optimize.UglifyJsPlugin(),
+    plugins:[        
+        // Note: setting compress: false in UglifyJsPlugin is a temporary fix for an UglifyJs bug... 
+        // https://github.com/webpack/webpack/issues/4394
+        // https://github.com/mishoo/UglifyJS2/issues/1516
+        new webpack.optimize.UglifyJsPlugin({ 
+            sourceMap: true,          
+            compress: false  
+        }),
     ]
 };
