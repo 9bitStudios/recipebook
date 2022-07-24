8e3a03a1c1567ab3cfea23df4d137b398f480aac
9bit Studios <9bitstudios@gmail.com>
2022-07-24 06:52:38 -0700

    Updating MySQL connection password...

--- utilities/SQL.js
+++ utilities/SQL.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'root',
     database: 'recipebook',
     multipleStatements: true  
 });
